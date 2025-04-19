031b46ec72dc10c70cfeda09caff7ba74cc2d3ba
Mohamed Zakaria <113525757+mohamed-zakariya@users.noreply.github.com>
2025-04-19 23:35:31 +0200

    Revert "Login 1"

--- insight-hub-dashboard/src/app/app.routes.ts
+++ insight-hub-dashboard/src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import { SignInComponent } from './components/auth/sign-in/sign-in.component';
 
-export const routes: Routes = [
-    {
-        path: '',
-        component: SignInComponent
-    }
-];
+export const routes: Routes = [];
