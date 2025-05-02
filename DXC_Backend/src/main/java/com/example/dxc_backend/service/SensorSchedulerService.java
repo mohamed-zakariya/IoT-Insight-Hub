f3a616ac8e5ec1f3185180565d48770475ce401e
radwan-mohamed <75395442+radwan-mohamed@users.noreply.github.com>
2025-05-02 20:42:26 +0300

    Light of the street sensor works well hurray
    weoorks well with generation OwOOwOOwO~_~^o^~_~

--- DXC_Backend/src/main/java/com/example/dxc_backend/service/SensorSchedulerService.java
+++ DXC_Backend/src/main/java/com/example/dxc_backend/service/SensorSchedulerService.java
@@ -1,8 +1,10 @@
 package com.example.dxc_backend.service;
 
 import com.example.dxc_backend.controller.AirPollutionSensorDataController;
+import com.example.dxc_backend.controller.StreetLightSensorDataController;
 import com.example.dxc_backend.controller.TrafficSensorDataController;
 import com.example.dxc_backend.model.AirPollutionSensorData;
+import com.example.dxc_backend.model.StreetLightSensorData;
 import com.example.dxc_backend.model.TrafficSensorData;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -16,15 +18,24 @@ public class SensorSchedulerService {
     private final AirPollutionSensorDataService airPollutionSensorDataService;
     private final AirPollutionSensorDataController airPollutionSensorDataController;
 
+    private final StreetLightSensorDataService streetLightSensorDataService;
+    private final StreetLightSensorDataController streetLightSensorDataController;
+
+
+
     // Constructor to inject the dependencies for both traffic and air pollution services/controllers
     public SensorSchedulerService(TrafficSensorDataService trafficSensorDataService,
                                   TrafficSensorDataController trafficSensorDataController,
                                   AirPollutionSensorDataService airPollutionSensorDataService,
-                                  AirPollutionSensorDataController airPollutionSensorDataController) {
+                                  AirPollutionSensorDataController airPollutionSensorDataController,
+                                  StreetLightSensorDataService streetLightSensorDataService,
+                                  StreetLightSensorDataController streetLightSensorDataController  ) {
         this.trafficSensorDataService = trafficSensorDataService;
         this.trafficSensorDataController = trafficSensorDataController;
         this.airPollutionSensorDataService = airPollutionSensorDataService;
         this.airPollutionSensorDataController = airPollutionSensorDataController;
+        this.streetLightSensorDataService = streetLightSensorDataService;
+        this.streetLightSensorDataController = streetLightSensorDataController;
     }
 
     @Scheduled(fixedRate = 20000) // Runs every 5 minutes (300,000 ms)
@@ -40,4 +51,14 @@ public class SensorSchedulerService {
         System.out.println("Generated Air Pollution Sensor Data: " + randomData.toString());
         airPollutionSensorDataController.createSensorData(randomData); // Call the POST function
     }
+
+    @Scheduled(fixedRate = 20000) // Runs every 5 minutes (300,000 ms)
+    public void generateAndPostStreetLightSensorData() {
+        StreetLightSensorData randomData = streetLightSensorDataService.generateRandomStreetLightSensorData();
+        System.out.println("Generated Traffic Sensor Data: " + randomData.toString());
+        streetLightSensorDataController.createSensorData(randomData); // Call the POST function
+    }
+
+
+
 }
