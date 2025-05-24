c6822ac30a02ae517d2bea87497a024e0441d415
radwan-mohamed <75395442+radwan-mohamed@users.noreply.github.com>
2025-05-24 13:11:29 +0300

    Revert "add filtering and sorting from database"
    This reverts commit 60cfeb36a80c2c9d3a12803f7fe2d0f5beaa2ea0.

--- DXC_Backend/src/main/java/com/example/dxc_backend/controller/TrafficSensorDataController.java
+++ DXC_Backend/src/main/java/com/example/dxc_backend/controller/TrafficSensorDataController.java
@@ -2,13 +2,9 @@ package com.example.dxc_backend.controller;
 
 import com.example.dxc_backend.model.TrafficSensorData;
 import com.example.dxc_backend.service.TrafficSensorDataService;
-import org.springframework.data.domain.Page;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -23,17 +19,8 @@ public class TrafficSensorDataController {
     }
 
     @GetMapping
-    public Page<TrafficSensorData> getFilteredTrafficSensorData(
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime timestampStart,
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime timestampEnd,
-            @RequestParam(required = false) String location,
-            @RequestParam(required = false) String congestionLevel,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size,    // number of records returned per page
-            @RequestParam(defaultValue = "timestamp") String sortBy,
-            @RequestParam(defaultValue = "DESC") String sortDirection
-    ) {
-        return service.getFilteredData(timestampStart, timestampEnd, location, congestionLevel, page, size, sortBy, sortDirection);
+    public List<TrafficSensorData> getAllTrafficSensorData() {
+        return service.getAllTrafficSensorData();
     }
 
     @GetMapping("/{id}")
