55be5dd2e9f125b128bab6b5409086ac9af8e946
bassel27 <bassel_sabour@hotmail.com>
2025-07-09 20:57:00 +0300

    test filter by location

--- insight-hub-dashboard/e2e/air-pollution.spec.ts
+++ insight-hub-dashboard/e2e/air-pollution.spec.ts
@@ -8,22 +8,19 @@ test.describe("Air Pollution Dashboard", () => {
   test("Filter works correctly", async ({ page }) => {
     await page.goto(FRONTEND_BASE_URL + "/street-light-management");
     await page.getByRole('combobox', { name: 'Locations' }).click();
-    const firstOption = page.locator('.mat-mdc-option.mdc-list-item.mat-mdc-option-multiple').nth(2);
+    const firstOption = page.locator('.mat-mdc-option').nth(2);
     const optionText = await firstOption.textContent();
     await expect(firstOption).toBeVisible();  // this waits 5 seconds
     await firstOption.click();
-    console.log(`Selected option: ${optionText}`);
     await page.pause();
 
     const locationCells = page.locator('td.cdk-column-location');
 
     const count = await locationCells.count();
-    console.log(`Found ${count} location cells.`);
 
     for (let i = 0; i < count; i++) {
       const cellText = await locationCells.nth(i).textContent();
       if (cellText !== null) {
-        console.log(`Cell ${i} text: "${cellText.trim()}"`);
         expect(cellText.trim()).toBe(optionText?.trim());
       } else {
         throw new Error(`Cell ${i} textContent is null`);
@@ -31,7 +28,8 @@ test.describe("Air Pollution Dashboard", () => {
     }
   });
 
-  // test("Chart values match table entries", async ({ page }) => {
-  //   await page.goto(FRONTEND_BASE_URL + "/air-pollution-monitoring");
-  // });
+  test("Chart values match table entries", async ({ page }) => {
+    await page.goto(FRONTEND_BASE_URL + "/air-pollution-monitoring");
+  });
+
 });
